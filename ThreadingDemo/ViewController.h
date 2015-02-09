7408d78d9dc4e0b255d276faf5755c94b85a6e4a
Mangesh Tekale <mangesh20@gmail.com>
2015-02-10 00:31:40 +0530

    MultithreadingDemo

--- ThreadingDemo/ViewController.h
+++ ThreadingDemo/ViewController.h
@@ -10,6 +10,12 @@
 
 @interface ViewController : UIViewController
 
-
+@property (weak, nonatomic) IBOutlet UILabel *lblCount1;
+@property (weak, nonatomic) IBOutlet UILabel *lblCount2;
+@property (weak, nonatomic) IBOutlet UILabel *lblCount3;
+@property (weak, nonatomic) IBOutlet UILabel *lblCount4;
+@property (weak, nonatomic) IBOutlet UILabel *lblCount5;
+- (IBAction)btnSerialTaskButtonTapped:(id)sender;
+- (IBAction)btnConcurrentTaskTapped:(id)sender;
 @end
 
--- ThreadingDemo/ViewController.m
+++ ThreadingDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#define kColorGreen [UIColor colorWithRed:143/255.0 green:199/255.0 blue:107/255.0 alpha:1]
+#define kColorRed [UIColor colorWithRed:254/255.0 green:129/255.0 blue:70/255.0 alpha:1]
+@interface ViewController (){
+    dispatch_queue_t backgroundQueue;
+}
 
 @end
 
@@ -24,4 +27,160 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnSerialTaskButtonTapped:(id)sender {
+    backgroundQueue = dispatch_queue_create("Global", nil);
+        [self allTasks];
+    
+    
+}
+
+- (IBAction)btnConcurrentTaskTapped:(id)sender {
+    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+        [self allTasksParallel];
+    
+}
+
+- (IBAction)btnResetAllTapped:(id)sender {
+    self.lblCount1.text = @"0";
+    self.lblCount2.text = @"0";
+    self.lblCount3.text = @"0";
+    self.lblCount4.text = @"0";
+    self.lblCount5.text = @"0";
+    [self.lblCount1 setBackgroundColor:kColorRed];
+    [self.lblCount2 setBackgroundColor:kColorRed];
+    [self.lblCount3 setBackgroundColor:kColorRed];
+    [self.lblCount4 setBackgroundColor:kColorRed];
+    [self.lblCount5 setBackgroundColor:kColorRed];
+}
+- (void)allTasks{
+    
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTask]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount1.text = @"1";
+                [self.lblCount1 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTask]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount2.text = @"1";
+                [self.lblCount2 setBackgroundColor:kColorGreen];
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTask]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount3.text = @"1";
+                [self.lblCount3 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTask]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount4.text = @"1";
+                [self.lblCount4 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTask]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount5.text = @"1";
+                [self.lblCount5 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+
+    
+}
+
+- (void)allTasksParallel{
+    
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTaskParallel]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount1.text = @"1";
+                [self.lblCount1 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTaskParallel]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount2.text = @"1";
+                [self.lblCount2 setBackgroundColor:kColorGreen];
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTaskParallel]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount3.text = @"1";
+                [self.lblCount3 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTaskParallel]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount4.text = @"1";
+                [self.lblCount4 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+    dispatch_async(backgroundQueue, ^{
+        if ([self timeConsumingTaskParallel]) {
+            dispatch_sync(dispatch_get_main_queue(), ^{
+                self.lblCount5.text = @"1";
+                [self.lblCount5 setBackgroundColor:kColorGreen];
+                
+            });
+        }
+    });
+
+
+ 
+
+ 
+    
+    
+    
+}
+static NSInteger constant =19999;
+
+- (BOOL)timeConsumingTask{
+    BOOL sucess =YES;
+    
+    ////////Time Consuming Task/////////////
+    NSInteger idx =0;
+        for (idx = 0; idx<constant; idx++) {
+    NSLog(@"%ld",(long)idx);
+    
+        }
+    
+    return sucess;
+}
+- (BOOL)timeConsumingTaskParallel{
+    BOOL sucess =YES;
+    
+    ////////Time Consuming Task/////////////
+    dispatch_apply(constant, backgroundQueue, ^(size_t idx) {
+                NSLog(@"%ld",(long)idx);
+    
+            });
+    
+    
+    return sucess;
+}
 @end
