d1afd2d1a92e91383500fa1cbc490aee07844fee
Mangesh Tekae <mangesh20@gmail.com>
2015-08-24 17:23:14 +0530

    Added .m file

--- ThreadingDemo/ViewController.m
+++ ThreadingDemo/ViewController.m
@@ -10,8 +10,8 @@
 #define kColorGreen [UIColor colorWithRed:143/255.0 green:199/255.0 blue:107/255.0 alpha:1]
 #define kColorRed [UIColor colorWithRed:254/255.0 green:129/255.0 blue:70/255.0 alpha:1]
 @interface ViewController (){
-    dispatch_queue_t backgroundQueue;
-    NSInteger count;
+        dispatch_queue_t backgroundQueue;
+        NSInteger count;
 }
 
 @end
@@ -31,42 +31,42 @@
 - (IBAction)btnSerialTaskButtonTapped:(id)sender {
     backgroundQueue = dispatch_queue_create("Global", nil);
     count = 1;
-        [self allTasks];
+    [self allTasks];
     
     
 }
 
 - (IBAction)btnConcurrentTaskTapped:(id)sender {
     backgroundQueue = dispatch_queue_create("Global", nil);
-
-//    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
+    
+    //    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0);
     count =1;
     dispatch_async(backgroundQueue, ^{
         
         [self allTasksParallel];
-
+        
     });
     
 }
 
 - (IBAction)btnResetAllTapped:(id)sender {
-    self.lblCount1.text = @"0";
-    self.lblCount2.text = @"0";
-    self.lblCount3.text = @"0";
-    self.lblCount4.text = @"0";
-    self.lblCount5.text = @"0";
-    [self.lblCount1 setBackgroundColor:kColorRed];
-    [self.lblCount2 setBackgroundColor:kColorRed];
-    [self.lblCount3 setBackgroundColor:kColorRed];
-    [self.lblCount4 setBackgroundColor:kColorRed];
-    [self.lblCount5 setBackgroundColor:kColorRed];
+        self.lblCount1.text = @"0";
+        self.lblCount2.text = @"0";
+        self.lblCount3.text = @"0";
+        self.lblCount4.text = @"0";
+        self.lblCount5.text = @"0";
+        [self.lblCount1 setBackgroundColor:kColorRed];
+        [self.lblCount2 setBackgroundColor:kColorRed];
+        [self.lblCount3 setBackgroundColor:kColorRed];
+        [self.lblCount4 setBackgroundColor:kColorRed];
+        [self.lblCount5 setBackgroundColor:kColorRed];
 }
 - (void)allTasks{
     
     dispatch_async(backgroundQueue, ^{
         if ([self timeConsumingTask]) {
             dispatch_sync(dispatch_get_main_queue(), ^{
-            
+                
                 self.lblCount1.text = [NSString stringWithFormat:@"%ld",(long)count++];
                 [self.lblCount1 setBackgroundColor:kColorGreen];
                 
@@ -108,14 +108,14 @@
             });
         }
     });
-
+    
     
 }
 
 - (void)allTasksParallel{
-
+    
     ///1
-//    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+    //    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
     dispatch_async(backgroundQueue, ^{
         if ([self timeConsumingTaskParallel]) {
             dispatch_sync(dispatch_get_main_queue(), ^{
@@ -125,9 +125,9 @@
             });
         }
     });
-
+    
     ///2
-//    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+    //    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
     dispatch_async(backgroundQueue, ^{
         if ([self timeConsumingTaskParallel]) {
             dispatch_sync(dispatch_get_main_queue(), ^{
@@ -138,7 +138,7 @@
     });
     
     ///3
-//    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    //    backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
     dispatch_async(backgroundQueue, ^{
         if ([self timeConsumingTaskParallel]) {
             dispatch_sync(dispatch_get_main_queue(), ^{
@@ -172,16 +172,6 @@
         }
     });
     
-    
-
-
-
- 
-
- 
-    
-    
-    
 }
 static NSInteger constant =9999;
 
@@ -190,10 +180,10 @@ static NSInteger constant =9999;
     
     ////////Time Consuming Task/////////////
     NSInteger idx =0;
-        for (idx = 0; idx<constant; idx++) {
-    NSLog(@"%ld",(long)idx);
-    
-        }
+    for (idx = 0; idx<constant; idx++) {
+        NSLog(@"%ld",(long)idx);
+        
+    }
     
     return sucess;
 }
@@ -204,7 +194,7 @@ static NSInteger constant =9999;
     dispatch_apply(constant, backgroundQueue, ^(size_t idx) {
         
         NSLog(@"%ld",(long)idx);
-    
+        
     });
     
     
